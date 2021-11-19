7a0aaf595b9b3c2902c5c831235c8b5491c0cfc8
Magne Rekdal <mrk@dips.no>
2021-11-19 15:46:52 +0100

    Standardisert formatering.

--- Utils/Emetra.EnumUtils.pas
+++ Utils/Emetra.EnumUtils.pas
@@ -7,40 +7,40 @@ uses
 
 type
   /// <summary>
-  ///   Exception is thrown when a string can not be converted to the requested
-  ///   enumeration type.
+  /// Exception is thrown when a string can not be converted to the requested
+  /// enumeration type.
   /// </summary>
   /// <remarks>
-  ///   <para>
-  ///     More on enumerated types here:
-  ///   </para>
-  ///   <para>
-  ///     <see href="http://docwiki.embarcadero.com/RADStudio/Tokyo/en/Simple_Types_(Delphi)#Enumerated_Types_with_Explicitly_Assigned_Ordinality">
-  ///     Enumerated Types</see>
-  ///   </para>
-  ///   <para>
-  ///     For some reason, Enumerated types with expilcitly assigned
-  ///     ordinalities have no RTTI, and using them with TEnumMapper will
-  ///     also throw this exception. <br /><br />
-  ///   </para>
+  /// <para>
+  /// More on enumerated types here:
+  /// </para>
+  /// <para>
+  /// <see href="http://docwiki.embarcadero.com/RADStudio/Tokyo/en/Simple_Types_(Delphi)#Enumerated_Types_with_Explicitly_Assigned_Ordinality">
+  /// Enumerated Types</see>
+  /// </para>
+  /// <para>
+  /// For some reason, Enumerated types with expilcitly assigned
+  /// ordinalities have no RTTI, and using them with TEnumMapper will
+  /// also throw this exception. <br /><br />
+  /// </para>
   /// </remarks>
   EEnumMapperError = class( Exception );
 
   /// <summary>
-  ///   This class improves a little bit on the standard RTTI functions by
-  ///   automatically stripping underscores from enum values (typically when
-  ///   read from JSON data). It can also add a prefix to the string before
-  ///   trying to convert it. Otherwise, just use value := TRttiEnumerationType.GetValue<T>(string);
+  /// This class improves a little bit on the standard RTTI functions by
+  /// automatically stripping underscores from enum values (typically when
+  /// read from JSON data). It can also add a prefix to the string before
+  /// trying to convert it. Otherwise, just use value := TRttiEnumerationType.GetValue<T>(string);
   /// </summary>
   /// <example>
-  ///   A string like "first_value" can be automatically converted to
-  ///   enFirstValue representing a member in a Delphi enumeration class that
-  ///   follows standard Delphi naming conventions.
+  /// A string like "first_value" can be automatically converted to
+  /// enFirstValue representing a member in a Delphi enumeration class that
+  /// follows standard Delphi naming conventions.
   /// </example>
   TEnumMapper = class
   public
     /// <summary>
-    ///   Simple wrapper for similar functionality in RTTI.
+    /// Simple wrapper for similar functionality in RTTI.
     /// </summary>
     class function GetName<T>( const AValue: T ): string;
     /// <summary>
@@ -87,8 +87,7 @@ type
     /// the prefix if ASampleValue.
     /// </remarks>
     class function GetValue<T { : enum } >( const AInput: string; const ASampleValue: T ): integer; overload;
-
-    class function GetValueNullable<T { : enum }>( const AInput: string; const AEnumPrefix: string = '' ): integer;
+    class function GetValueNullable<T { : enum } >( const AInput: string; const AEnumPrefix: string = '' ): integer;
   end;
 
 implementation
@@ -129,18 +128,14 @@ begin
   if not Assigned( enumTypeInfo ) then
     raise EEnumMapperError.CreateFmt( 'Failed to retrieve TypeInfo for Enum (%s)', [AInput] );
   Result := UNASSIGNED_ENUM;
-
   trimmedInput := AInput.Trim;
-
   { Avoid reallocation of string as it grows, set to max size first }
   SetLength( extractedString, Length( trimmedInput ) );
   extractedString := EmptyStr;
-
   { Add characters except whitespace and underscores }
   for currentChar in trimmedInput do
     if not CharInSet( currentChar, [#9, #10, #13, '_'] ) then
       extractedString := extractedString + currentChar;
-
   { Try to get value }
   Result := GetEnumValue( enumTypeInfo, AEnumPrefix + extractedString );
   if Result = UNASSIGNED_ENUM then
@@ -158,13 +153,12 @@ begin
 
   if Length( AInput.Trim ) = 0 then
   begin
-    //value was null
-    Result := GetValue<T>( 'null' , AEnumPrefix );
+    // value was null
+    Result := GetValue<T>( 'null', AEnumPrefix );
     exit;
   end;
 
   Result := GetValue<T>( AInput, AEnumPrefix );
 end;
-
 
 end.
