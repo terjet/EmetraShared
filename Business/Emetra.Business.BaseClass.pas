9eecf9f3e00fdfb0906e92bec566cd312c1198fc
Magne Rekdal <magne@rekdal.no>
2022-07-19 12:40:23 +0200

    #505531: Added ny baseclass to replace TBusiness, with reference counting and warnings if there are methods making the class usable except through it's interface.

--- Business/Emetra.Business.BaseClass.pas
+++ Business/Emetra.Business.BaseClass.pas
@@ -40,10 +40,34 @@ type
     property Log: ILog read fLog;
   end;
 
+{$TYPEINFO ON}
+
+  TCustomBusinessReferenceCounted = class( TInterfacedObject )
+  strict private
+    fLog: ILog;
+  protected
+    function MethodAllowed( const AMethodName: string ): boolean;
+    procedure CheckAssigned( const AInterface: IInterface; const ANameOfInterface: string );
+    procedure EnterMethod( const AProcName: string );
+    procedure LeaveMethod( const AProcName: string );
+    procedure VerifyConstructorParameters; dynamic;
+    procedure VerifyVisibility( const ATypeInfo: Pointer );
+    { Properties }
+    property Log: ILog read fLog;
+  public
+    { Initialization }
+    constructor Create( const ALog: ILog ); reintroduce;
+    procedure BeforeDestruction; override;
+  end;
+
 implementation
 
 uses
-  System.SysUtils;
+  System.SysUtils, System.Rtti, System.TypInfo;
+
+var
+  BaseMethods: TStringList;
+  ctx: TRttiContext;
 
 constructor TCustomBusiness.Create( const ALog: ILog );
 begin
@@ -85,5 +109,146 @@ begin
 end;
 
 {$ENDREGION}
+{ TCustomBusinessReferenceCounted }
+
+constructor TCustomBusinessReferenceCounted.Create( const ALog: ILog );
+begin
+  inherited Create;
+  fLog := ALog;
+end;
+
+procedure TCustomBusinessReferenceCounted.EnterMethod( const AProcName: string );
+begin
+  fLog.EnterMethod( Self, AProcName );
+end;
+
+procedure TCustomBusinessReferenceCounted.LeaveMethod( const AProcName: string );
+begin
+  fLog.LeaveMethod( Self, AProcName );
+end;
+
+function TCustomBusinessReferenceCounted.MethodAllowed( const AMethodName: string ): boolean;
+var
+  foundAt: Integer;
+begin
+  Result := BaseMethods.Find( AMethodName, foundAt );
+end;
+
+procedure TCustomBusinessReferenceCounted.VerifyConstructorParameters;
+begin
+  CheckAssigned( fLog, 'Log' );
+end;
+
+procedure TCustomBusinessReferenceCounted.VerifyVisibility( const ATypeInfo: Pointer );
+var
+  baseTypeInfo: Pointer;
+  ctx: TRttiContext;
+  ourType: TRttiType;
+  baseType: TRttiType;
+  ourProperty: TRttiProperty;
+  ourMethod: TRttiMethod;
+begin
+  { Get basic info }
+  baseTypeInfo := TypeInfo( TCustomBusinessReferenceCounted );
+  baseType := ctx.GetType( baseTypeInfo );
+  { Get info for this type }
+  ourType := ctx.GetType( ATypeInfo );
+  { Check that now new methods are added }
+  for ourMethod in ourType.GetMethods do
+  begin
+    if ( baseType.MethodAddress( ourMethod.Name ) = nil ) and ( not MethodAllowed( ourMethod.Name ) ) then
+    begin
+      if ourMethod.Visibility > mvProtected then
+        Log.Event( '%s.%s: Visibility is too high for this method.', [ClassName, ourMethod.Name], ltError )
+      else
+        Log.SilentSuccess( '%s.%s: Visibility OK for this method.', [ClassName, ourMethod.Name] );
+    end
+    else
+      Log.Event( '%s.%s: Method found in base class or in list of allowed methods.', [ClassName, ourMethod.Name] );
+  end;
+
+  for ourProperty in ourType.GetProperties do
+  begin
+    if baseType.GetProperty( ourProperty.Name ) = nil then
+    begin
+      if ourProperty.Visibility > mvProtected then
+        Log.Event( '%s.%s: Visibility too high', [ClassName, ourProperty.Name], ltError )
+      else
+        Log.SilentSuccess( '%s.%s: Visibility OK for this property.', [ClassName, ourProperty.Name] );
+    end
+    else
+      Log.Event( '%s.%s: Property found in base class or in list of allowed properties.', [ClassName, ourProperty.Name] );
+  end;
+end;
+
+procedure TCustomBusinessReferenceCounted.BeforeDestruction;
+begin
+  Log.Event( '%s.BeforeDestruction called.', [ClassName] );
+  inherited;
+end;
+
+procedure TCustomBusinessReferenceCounted.CheckAssigned( const AInterface: IInterface; const ANameOfInterface: string );
+const
+  PROC_NAME = 'CheckAssigned';
+  ERR_MSG   = LOG_STUB_STRING + 'The interface is not assigned.';
+var
+  errorMessage: string;
+begin
+  if not Assigned( AInterface ) then
+  begin
+    errorMessage := Format( ERR_MSG, [ClassName, PROC_NAME, ANameOfInterface] );
+    if Assigned( fLog ) then
+      fLog.SilentError( errorMessage )
+    else if Assigned( GlobalLog ) then
+      GlobalLog.SilentError( errorMessage );
+    raise EArgumentNilException.Create( errorMessage );
+  end;
+end;
+
+initialization
+
+BaseMethods := TStringList.Create( TDuplicates.dupError, true, false );
+with BaseMethods do
+begin
+  { These are the methods that are found in TInterfacedObject }
+  Add( 'AfterConstruction' );
+  Add( 'BeforeDestruction' );
+  Add( 'ClassInfo' );
+  Add( 'ClassName' );
+  Add( 'ClassNameIs' );
+  Add( 'ClassParent' );
+  Add( 'ClassType' );
+  Add( 'CleanupInstance' );
+  Add( 'Create' );
+  Add( 'DefaultHandler' );
+  Add( 'Destroy' );
+  Add( 'Dispatch' );
+  Add( 'DisposeOf' );
+  Add( 'Equals' );
+  Add( 'FieldAddress' );
+  Add( 'Free' );
+  Add( 'FreeInstance' );
+  Add( 'GetHashCode' );
+  Add( 'GetInterface' );
+  Add( 'GetInterfaceEntry' );
+  Add( 'GetInterfaceTable' );
+  Add( 'InheritsFrom' );
+  Add( 'InitInstance' );
+  Add( 'InstanceSize' );
+  Add( 'MethodAddress' );
+  Add( 'MethodName' );
+  Add( 'NewInstance' );
+  Add( 'QualifiedClassName' );
+  Add( 'SafeCallException' );
+  Add( 'ToString' );
+  Add( 'UnitName' );
+  Add( 'UnitScope' );
+end;
+
+ctx := TRttiContext.Create;
+
+finalization
+
+ctx.Free;
 
 end.
