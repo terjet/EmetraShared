8f5fab633ed3f4b84c7532a9994c762a6018996e
Magne Rekdal <magne@rekdal.no>
2022-10-03 03:54:33 +0200

    Added som alternative names for addresses.

--- Utils/Emetra.Interfaces.Geography.pas
+++ Utils/Emetra.Interfaces.Geography.pas
@@ -3,21 +3,33 @@
 interface
 
 type
-  IGeoAddress = interface
+  IPhysicalAddress = interface
     ['{E47D815B-23E9-4721-BAE0-352DA7648E53}']
     { Property Accessors }
     function Get_City: string;
     function Get_PostCode: string;
     function Get_StreetAddress: string;
-    function Get_Phone: string;
     { Properties }
     property City: string read Get_City;
     property PostCode: string read Get_PostCode;
     property StreetAddress: string read Get_StreetAddress;
-    property Phone : string read Get_Phone;
-    { Kith Style naming }
+    { Kith Style naming for the same properties }
     property PostalCode: string read Get_PostCode;
     property StreetAdr: string read Get_StreetAddress;
+  end;
+
+  { Alternative names for IPhysicalAddress that may be more descriptive in some situations }
+
+  IGeoAddress = IPhysicalAddress;
+
+  IFolkeregisterAddress = IPhysicalAddress;
+
+  IContactInfo = interface( IPhysicalAddress )
+    ['{E47D815B-23E9-4721-BAE0-352DA7648E53}']
+    { Property Accessors }
+    function Get_Phone: string;
+    { Properties }
+    property Phone : string read Get_Phone;
   end;
 
   IStreetAddressDetail = interface['{E9FE221F-C57E-48B9-B900-C45AB34802F7}']
@@ -29,7 +41,7 @@ type
     property StreetNumber: string read Get_StreetNumber;
   end;
 
-  IGeoNamedSite = interface( IGeoAddress ) ['{DB0118C1-A58C-4630-9F60-51F0878467ED}']
+  IGeoNamedSite = interface( IContactInfo ) ['{DB0118C1-A58C-4630-9F60-51F0878467ED}']
     { Property Accessors }
     function Get_SiteName: string;
     { Properties }
